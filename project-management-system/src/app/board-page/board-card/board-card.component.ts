4f14c25aa858e459c1748bb8c71cdfd368aa603a
Kate <ekaterina21051988@yandex.by>
2023-07-25 12:08:25 +0300

    feat: created board-page

---
+++ project-management-system/src/app/board-page/board-card/board-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { BoardComponent} from "../../board/board.component";
+
+@Component({
+  selector: 'app-board-card',
+  templateUrl: './board-card.component.html',
+  styleUrls: ['./board-card.component.css']
+})
+export class BoardCardComponent {
+
+
+}
