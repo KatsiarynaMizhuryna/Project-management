e492820ca3bd7e94e8393ea0eaba35c6633704b9
Kate <ekaterina21051988@yandex.by>
2023-07-25 14:53:31 +0300

    feat: added class and methods for modal.service

--- project-management-system/src/app/_services/modal.service.ts
+++ project-management-system/src/app/_services/modal.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { MatDialog} from "@angular/material/dialog";
+import { DialogComponent} from "../dialog/dialog.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
+
+  openConfirmDialog(message: string){
+    return this.dialog.open(DialogComponent,{
+      width: '390px',
+      panelClass: 'confirm-dialog-container',
+      disableClose: true,
+      position: { top: "10px" },
+      data :{
+        message : message
+      }
+    });
+  }
 }
