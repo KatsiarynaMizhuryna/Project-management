40a22fcc007562b9d04120d0a45296196fb5e658
Kate <ekaterina21051988@yandex.by>
2023-07-25 12:17:57 +0300

    feat: added classes and functions for creating new boards and getting boards from mongo

--- project-management-system/src/app/board/board.component.ts
+++ project-management-system/src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
